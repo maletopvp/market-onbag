2204d11ff41195cb6c83cee3b754d92b4b5df8ff
Gabriel <gabriel_theguitar@hotmail.com>
2019-10-22 00:13:11 -0300

    Realizando configuções essenciais para o funcionamento do sistema

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'marketOnBag'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_URL = '/static/css/'
+STATIC_URL = '/static/js/'
+STATIC_URL = '/static/img/'
